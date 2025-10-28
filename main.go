119952f494dc8f69830446bb5fadbe9863aff41b
Nishankumar <84121106+Nishankumar@users.noreply.github.com>
2025-10-28 13:22:42 +0530

    Create main.go

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+type Request struct {
+	Name string `json:"name"`
+}
+
+type Response struct {
+	Message string `json:"message"`
+}
+
+func handler(ctx context.Context, req Request) (Response, error) {
+	message := fmt.Sprintf("Hello, %s! This is a Go Lambda deployed via GitHub Actions 🚀", req.Name)
+	return Response{Message: message}, nil
+}
+
+func main() {
+	lambda.Start(handler)
+}
